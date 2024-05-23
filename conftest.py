266b79da1c5ef3b63fb357157e30da557408b582
Priyanka H S <priyasuresh2717@gmail.com>
2024-05-23 10:49:08 +0530

    conftest updated

--- conftest.py
+++ conftest.py
@@ -3,12 +3,14 @@ import pendulum
 import pytest
 from allure_commons.types import AttachmentType
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope="session")
 @allure.title("Prepare for the test")
 def driver_instance():
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.delete_all_cookies()
     driver.implicitly_wait(30)
     driver.maximize_window()
